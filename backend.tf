acee253df660b3465d395875b7cf06e16537c15f
kateryna_kloskowska <k.kloskowska@favbet.tech>
2025-11-03 18:22:29 +0100

    lesson-5: terraform

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-avoo-bucket-terraform"
+    key            = "lesson-5/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
